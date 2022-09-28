84bf7354f47c64761b5f3f1dffe03fe8a04712a5
Kanan172411 <kanangarayev02@gmail.com>
2022-09-28 13:32:32 +0400

    Repository created

---
+++ CommandsServices/CommandsServices/Data/CommandRepo.cs
@@ -0,0 +1,62 @@
+﻿using CommandsServices.Models;
+
+namespace CommandsServices.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly AppDbContext _context;
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CreateCommand(int platformId, Command command)
+        {
+            if(command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+
+            command.PlatformId = platformId;
+
+            _context.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _context.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Command> GetAllCommandsForPlatform(int platformId)
+        {
+            return (_context.Commands
+                .Where(x => x.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name));
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return (_context.Commands
+                .Where( c => c.PlatformId == platformId &&  c.Id == commandId)
+                .FirstOrDefault());
+        }
+
+        public bool PlatformExist(int platformId)
+        {
+            return (_context.Platforms.Any(x => x.Id == platformId));
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ CommandsServices/CommandsServices/Data/ICommandRepo.cs
@@ -0,0 +1,24 @@
+﻿using CommandsServices.Models;
+
+namespace CommandsServices.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+
+        // Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform platform);
+        bool PlatformExist(int platformId);
+
+        //Commands
+        IEnumerable<Command> GetAllCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+
+
+
+
+    }
+}
